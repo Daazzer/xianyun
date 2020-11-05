ecb0d35949c3f3c884707c5ceb543c2796e69bcc
Daazzer <Daazzer@github.com>
2020-11-05 16:20:26 +0800

    将全局样式合并到 common.scss 文件中，修改配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,6 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     'element-ui/lib/theme-chalk/index.css',
-    'assets/common.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -45,8 +44,8 @@ export default {
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: "http://157.122.54.189:9095" // 新增备用地址
-    // baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径
+    baseURL: "http://157.122.54.189:9095" // 在线数据库
+    // baseURL: "http://localhost:1337" // 本地数据库
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
