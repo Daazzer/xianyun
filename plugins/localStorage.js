c2cdb5d2d408a1557b525a1c160155f9ccee427f
Daazzer <Daazzer@github.com>
2020-11-07 17:23:33 +0800

    添加 vuex-persistedstate 注释说明

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -4,6 +4,11 @@ export default ({ store }) => {
   window.onNuxtReady(() => {
     createPersistedState({
       key: 'xianyun', // 读取本地存储的数据到 store
+      /**
+       * 利用 paths 保存部分状态到本地
+       * @see {@link https://github.com/robinvdvleuten/vuex-persistedstate#example-with-vuex-modules Github@vuex-persistedstate}
+       */
+      paths: ['user.userInfo']
     })(store)
   })
 }
