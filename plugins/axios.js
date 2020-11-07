b3cfa0408c07c58a033de636eb3c4f209dc289a7
Daazzer <Daazzer@github.com>
2020-11-07 17:24:17 +0800

    添加 api 地址的说明

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,12 @@
+// 注意，@nuxtjs/axios 默认就集成了原生 axios，可以直接 import axios from 'axios'
 import api from './api'
 
 export default ({ $axios }, inject) => {
-  $axios.setBaseURL('http://157.122.54.189:9095') // 在线数据库
-  // $axios.setBaseURL('http://localhost:1337') // 本地数据库
+  /**
+   * @see {@link http://157.122.54.189:9095} 在线数据库
+   * @see {@link http://localhost:1337} 本地数据库
+   */
+  $axios.setBaseURL('http://157.122.54.189:9095')
 
   // 可以设置请求拦截器
 
