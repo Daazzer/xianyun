203c24bbaf93894700a2d39d95f8a97b1f13577c
Daazzer <Daazzer@github.com>
2020-11-08 10:01:43 +0800

    新增机票列表页路由

---
+++ pages/air/flights.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="flights">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
+
+        <!-- 航班头部布局 -->
+        <div>机票列表</div>
+
+        <!-- 航班信息 -->
+        <div></div>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Flights',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.flights {
+  width: 1000px;
+  margin: 20px auto;
+  &-content {
+    width: 745px;
+    font-size: 14px;
+  }
+}
+
+.aside {
+  width: 240px;
+}
+</style>
