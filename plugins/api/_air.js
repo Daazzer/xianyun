d81c4cd5e79438895b82f21ad28d1a25aa716421
Daazzer <Daazzer@github.com>
2020-11-08 10:36:00 +0800

    封装机票列表接口

--- plugins/api/_air.js
+++ plugins/api/_air.js
@@ -8,5 +8,16 @@ export default axios => ({
    * @returns {Promise<Response>}
    */
   getAirsCity: params => handleAxiosRequest(axios.get('/airs/city', { params })),
-  getRecommendAirs: () => handleAxiosRequest(axios.get('/airs/sale'))
+  getRecommendAirs: () => handleAxiosRequest(axios.get('/airs/sale')),
+  /**
+   * 机票列表
+   * @param {Object} params
+   * @param {string} params.departCity 出发城市
+   * @param {string} params.departCode 出发城市代码
+   * @param {string} params.destCity 目标城市
+   * @param {string} params.destCode 目标城市代码
+   * @param {string} params.departDate 出发日期（例如：2019-05-01）
+   * @returns {Promise<Response>}
+   */
+  getAirs: params => handleAxiosRequest(axios.get('/airs', { params }))
 })
