43b4b08ab2e7072ecc67cf54fcf611c387ad78bb
Daazzer <Daazzer@github.com>
2020-11-08 18:23:56 +0800

    航班列表的分页展示

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -16,6 +16,23 @@
             :key="flight.id"
             :flight="flight"
           />
+          <!-- 分页 -->
+          <el-row type="flex" justify="center" style="margin-top: 10px">
+            <!-- size-change：切换条数时候触发 -->
+            <!-- current-change：选择页数时候触发 -->
+            <!-- current-page: 当前页数 -->
+            <!-- page-size：当前条数 -->
+            <!-- total：总条数 -->
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="pageIndex"
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="pageSize"
+              :total="flightsData.total"
+              layout="total, sizes, prev, pager, next, jumper"
+            />
+          </el-row>
         </div>
       </div>
 
@@ -33,8 +50,30 @@ export default {
   data () {
     return {
       flightsData: {}, // 航班总数据
-      flights: []     // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+      flights: [],     // 航班列表数据，用于循环 FlightsItem组件，单独出来是因为要分页
+      pageIndex: 1, // 当前页码
+      pageSize: 5,  // 显示条数
     }
+  },
+  methods: {
+    // 获取一段航班列表数据，设置每一页的航班列表数据
+    setFlights () {
+      const start = (this.pageIndex - 1) * this.pageSize
+      const end = start + this.pageSize
+      this.flights = this.flightsData.flights.slice(start, end)
+    },
+
+    // 切换条数时触发
+    handleSizeChange (pageSize) {
+      this.pageSize = pageSize
+      this.setFlights()
+    },
+
+    // 切换页数时触发
+    handleCurrentChange (pageIndex) {
+      this.pageIndex = pageIndex
+      this.setFlights()
+    },
   },
   async mounted () {
     const [err, res] = await this.$api.getAirs(this.$route.query)
@@ -44,7 +83,7 @@ export default {
     }
 
     this.flightsData = res.data
-    this.flights = this.flightsData.flights
+    this.setFlights()
   }
 }
 </script>
