7f46400282388c1009af74d5a866baed7154a641
Daazzer <Daazzer@github.com>
2020-11-08 12:23:01 +0800

    添加请求拦截，判断是否请求到了 token 路由，如果进入了才设置 token

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,15 +2,38 @@
 import api from './api'
 import { Message } from 'element-ui'
 
-export default ({ $axios, redirect }, inject) => {
+export default ({ $axios }, inject) => {
+  /**
+   * 判断是否请求到需要 token 的路由
+   * @param {string} url 请求地址段
+   * @returns {boolean} 是否匹配给定的地址段
+   */
+  const checkAuthUrl = url => (
+    /^\/comments(\/like)?/.test(url) ||
+    /^\/airorders(\/(pay|checkpay|\d+))?/.test(url) ||
+    /^\/posts(\/(star|like))?/.test(url) ||
+    /^\/upload/.test(url)
+  )
+
   /**
    * @see {@link http://157.122.54.189:9095} 在线数据库
    * @see {@link http://localhost:1337} 本地数据库
    */
   $axios.setBaseURL('http://157.122.54.189:9095')
 
+  // 请求拦截
+  $axios.onRequest(config => {
+    const xianyun = JSON.parse(localStorage.getItem('xianyun'))
+
+    if (xianyun && checkAuthUrl(config.url)) {
+      const token = xianyun.user.userInfo.token
+      config.headers.Authorization = 'Bearer ' + token
+    }
+  })
+
   // 可以设置请求拦截器
   $axios.onError(error => {
+    console.log(error)
     const { statusCode, message } = error.response.data
     if (statusCode === 400) {
       Message.warning(message)
