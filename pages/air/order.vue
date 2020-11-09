26137245f1944c5427f881379c81942eaa1930fb
Daazzer <Daazzer@github.com>
2020-11-09 11:42:57 +0800

    添加机票订单页，封装订单组件

---
+++ pages/air/order.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="order">
+    <el-row type="flex" justify="space-between">
+      <!-- 订单表单 -->
+      <OrderForm />
+
+      <!-- 侧边栏 -->
+      <div class="aside"></div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Order'
+}
+</script>
+
+<style lang="scss" scoped>
+.order {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
+</style>
