2843113ce78a0018b9f15aa2107345664c97085e
Daazzer <Daazzer@github.com>
2020-11-10 17:33:49 +0800

    封装提交机票订单 api

--- plugins/api/_air.js
+++ plugins/api/_air.js
@@ -5,9 +5,10 @@ export default axios => ({
    * 实时机票城市
    * @param {Object} params
    * @param {string} params.name 出发城市
-   * @returns {Promise<Response>}
+   * @returns {Promise}
    */
   getAirsCity: params => handleAxiosRequest(axios.get('/airs/city', { params })),
+  /** 获取机票推荐数据 */
   getRecommendAirs: () => handleAxiosRequest(axios.get('/airs/sale')),
   /**
    * 机票列表
@@ -17,15 +18,31 @@ export default axios => ({
    * @param {string} params.destCity 目标城市
    * @param {string} params.destCode 目标城市代码
    * @param {string} params.departDate 出发日期（例如：2019-05-01）
-   * @returns {Promise<Response>}
+   * @returns {Promise}
    */
   getAirs: params => handleAxiosRequest(axios.get('/airs', { params })),
   /**
    * 选择机票
    * @param {string} id 机票id
-   * @param {Object} params
+   * @param {Object} params 获取机票的参数对象
    * @param {string} params.seat_xid 座位id
-   * @returns {Promise<Response>}
+   * @returns {Promise}
    */
-  getAirsById: (id, params) => handleAxiosRequest(axios.get(`/airs/${id}`, { params }))
+  getAirsById: (id, params) => handleAxiosRequest(axios.get(`/airs/${id}`, { params })),
+
+  /**
+   * 提交机票订单
+   * @param {Object} data 参数对象
+   * @param {Object[]} data.users 用户列表
+   * @param {string} data.users[].username 用户名
+   * @param {string} data.users[].id 用户 id
+   * @param {string} data.insurances 保险 id
+   * @param {string} data.contactName 联系人名字
+   * @param {string} data.contactPhone 联系人电话
+   * @param {boolean} data.invoice 是否需要发票
+   * @param {string} data.seat_xid 座位 id
+   * @param {string} data.air 航班 id
+   * @returns {Promise}
+   */
+  submitAirOrders: data => handleAxiosRequest(axios.post('/airorders', data))
 })
