ab20bf38505e87e8dcad410a1d89d21e3118f322
Daazzer <Daazzer@github.com>
2020-11-10 09:18:53 +0800

    机票订单信息获取功能的实现

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -2,7 +2,7 @@
   <div class="order">
     <el-row type="flex" justify="space-between">
       <!-- 订单表单 -->
-      <OrderForm />
+      <OrderForm :infoData="infoData" />
 
       <!-- 侧边栏 -->
       <div class="aside"></div>
@@ -12,7 +12,26 @@
 
 <script>
 export default {
-  name: 'Order'
+  name: 'Order',
+  data () {
+    return {
+      // 机票信息
+      infoData: {
+        insurances: [] // 初始化保险数据
+      }
+    }
+  },
+  async mounted () {
+    const { id, seat_xid } = this.$route.query
+
+    const [err, res] = await this.$api.getAirsById(id, { seat_xid })
+
+    if (err) {
+      return this.$message.error('获取订单信息失败')
+    }
+
+    this.infoData = res.data
+  }
 }
 </script>
 
