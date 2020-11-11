ee491d4c2d59e937447c21e1e0724dca9383a235
Daazzer <Daazzer@github.com>
2020-11-11 09:54:24 +0800

    新增支付页

---
+++ pages/air/pay.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="pay">
+    <main>
+      <div class="pay-title">
+        支付总金额 <span class="pay-price">￥ 1000</span>
+      </div>
+      <div class="pay-main">
+        <h4>微信支付</h4>
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          class="pay-qrcode"
+        >
+          <div class="qrcode">
+            <!-- 二维码 -->
+            <canvas id="qrcode-stage"></canvas>
+            <p>请使用微信扫一扫</p>
+            <p>扫描二维码支付</p>
+          </div>
+          <div class="pay-example">
+            <img src="http://157.122.54.189:9093/images/wx-sweep2.jpg" alt="" />
+          </div>
+        </el-row>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pay'
+}
+</script>
+
+<style scoped lang="scss">
+.pay {
+  padding: 30px 0;
+  background: #f5f5f5;
+
+  main {
+    width: 1000px;
+    margin: 0 auto;
+
+    .pay-title {
+      text-align: right;
+      span {
+        font-size: 28px;
+        color: orangered;
+      }
+    }
+
+    .pay-main {
+      margin-top: 10px;
+      padding: 30px;
+      border-top: 5px orange solid;
+      background: #fff;
+
+      h4 {
+        margin-bottom: 10px;
+        font-size: 28px;
+        font-weight: normal;
+      }
+
+      .pay-qrcode {
+        padding: 0 80px;
+      }
+
+      .qrcode {
+        height: fit-content;
+        padding: 15px;
+        border: 1px #ddd solid;
+
+        #qrcode-stage {
+          $size: 200px;
+          width: $size;
+          height: $size;
+          margin-bottom: 10px;
+        }
+
+        p {
+          line-height: 2;
+          text-align: center;
+        }
+      }
+    }
+  }
+}
+</style>
