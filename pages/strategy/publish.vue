61a78938b2f266dce271f20342ab51ccbaf221ee
Daazzer <Daazzer@github.com>
2020-11-14 00:22:10 +0800

    发布页的初步样式布局

--- pages/strategy/publish.vue
+++ pages/strategy/publish.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="w">文章发布页</div>
+  <el-row class="publish w" type="flex" justify="space-between">
+    <el-col class="publish-content">
+      <h2>发表新攻略</h2>
+      <p class="publish-content__tips">分享你的个人游记，让更多人看到哦！</p>
+    </el-col>
+    <el-col class="publish-draft">
+      草稿
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -7,3 +15,22 @@ export default {
   name: 'StrategyPublish'
 }
 </script>
+
+<style lang="scss" scoped>
+.publish-content {
+  width: 750px;
+  h2 {
+    font-size: 22px;
+    font-weight: 400;
+    margin-bottom: 10px;
+  }
+  &__tips {
+    font-size: 12px;
+    color: #999;
+    margin-bottom: 10px;
+  }
+}
+.publish-draft {
+  width: 225px;
+}
+</style>
