406ed89aa4f1e5174b6dde1d5c4b50ddffa8910a
Daazzer <Daazzer@github.com>
2020-11-14 23:08:38 +0800

    评论楼层的基本递归布局

--- components/strategy/detail/CommentFloor.vue
+++ components/strategy/detail/CommentFloor.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="comment-floor">评论楼层</div>
+  <div class="comment-floor">
+    <!-- <CommentFloor /> -->
+    <div class="comment-floor__content">
+      <el-row class="comment-floor-info" type="flex" justify="space-between" align="middle">
+        <el-row type="flex" align="middle">
+          <span class="username">地球发动机</span>
+          <span class="time">2020-11-14 7:37</span>
+        </el-row>
+        <span class="floor-num">2</span>
+      </el-row>
+      <p class="comment-floor-message">你个沙雕</p>
+      <el-row type="flex">
+        <div class="comment-floor-pic">
+          <el-image src="http://157.122.54.189:9095/uploads/ef1727a000164749a0fe16cea86eaefe.jpg" />
+        </div>
+      </el-row>
+      <div class="comment-floor-reply">
+        <a href="javascript:;">回复</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +27,11 @@ export default {
   name: 'CommentFloor'
 }
 </script>
+
+<style lang="scss" scoped>
+.comment-floor {
+  padding: 2px;
+  border: 1px solid #ddd;
+  background-color: #f9f9f9;
+}
+</style>
