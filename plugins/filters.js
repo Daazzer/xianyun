d2ed36a5250fd5c1510c3a2a802f2c0ca32a035a
Daazzer <Daazzer@github.com>
2020-11-15 20:00:36 +0800

    时间格式化函数更改

--- plugins/filters.js
+++ plugins/filters.js
@@ -2,18 +2,23 @@ export function timeFormat (time) {
   if (isNaN(time)) {
     return '-- :'
   }
+
   const date = new Date(time)
   const MM = date.getMonth() + 1
   const DD = date.getDate()
+  const mm = date.getMinutes()
+
   const YYYYMMDD = [
     date.getFullYear(),
     MM < 10 ? `0${MM}` : MM,
     DD < 10 ? `0${DD}` : DD
   ].join('-')
+
   const hhmm = [
     date.getHours(),
-    date.getMinutes()
+    mm < 10 ? `0${MM}` : mm
   ].join(':')
+
   return YYYYMMDD + ' ' + hhmm
 }
 
