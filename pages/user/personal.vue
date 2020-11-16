37e712d3dfd6397cc58b15e7802c70426362e384
Daazzer <Daazzer@github.com>
2020-11-16 16:37:47 +0800

    新增个人主页路由

---
+++ pages/user/personal.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-row
+    class="w personal"
+    type="flex"
+    justify="center"
+    align="middle"
+  >个人主页，暂未开通</el-row>
+</template>
+
+<script>
+export default {
+  name: 'Personal'
+}
+</script>
+
+<style lang="scss" scoped>
+.personal {
+  color: #999;
+  height: 200px;
+}
+</style>
