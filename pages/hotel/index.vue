b0ad6043cea8c6020989f50aa401116e6d0df04c
Daazzer <Daazzer@github.com>
2020-11-17 11:33:30 +0800

    酒店列表页的面包屑路径

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>酒店</div>
+  <div class="hotel w">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>酒店</el-breadcrumb-item>
+      <el-breadcrumb-item>酒店预订</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
 </template>
 
 <script>
