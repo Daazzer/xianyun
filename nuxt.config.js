5899433eb7264427b0e689c87a9abda883183784
Daazzer <Daazzer@github.com>
2020-11-22 15:37:48 +0800

    更改进度条配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,12 +10,12 @@ export default {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'}
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      // { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'}
     ]
   },
 
-  loading: { color: '#fff' },
+  loading: { color: '#409eff' },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
