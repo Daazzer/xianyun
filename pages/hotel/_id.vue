cde45c6d110216716099a6a8c08a1432012448a9
Daazzer <Daazzer@github.com>
2020-12-31 13:07:22 +0800

    加载状态与空数据状态

--- pages/hotel/_id.vue
+++ pages/hotel/_id.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="hotel-detail w">
-    <el-breadcrumb>
-      <el-breadcrumb-item to="/hotel">酒店</el-breadcrumb-item>
-      <el-breadcrumb-item :to="{ path: '/hotel', query: { cityName: $store.state.hotel.locationCity || hotel.city.name } }">{{ hotel.real_city }}酒店</el-breadcrumb-item>
-      <el-breadcrumb-item>{{ hotel.name }}</el-breadcrumb-item>
-    </el-breadcrumb>
-    <HotelDetailNameInfo :hotel="hotel" />
-    <HotelDetailPicsInfo />
-    <HotelDetailProducts :hotelProductsTableData="hotel.products" />
-    <HotelDetailAreaInfo />
-    <HotelDetailAssets :hotel="hotel" />
-    <HotelDetailComments :hotel="hotel" />
+  <div>
+    <div
+      v-if="loadingHotel"
+      class="hotel-detail--loading"
+      v-loading="loadingHotel"
+      element-loading-spinner="iconfont icon-jiazaizhong"
+    ></div>
+    <div class="hotel-detail--none" v-else-if="noneHotelData">暂无酒店信息</div>
+    <div class="hotel-detail w" v-else>
+      <el-breadcrumb>
+        <el-breadcrumb-item to="/hotel">酒店</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: '/hotel', query: { cityName: $store.state.hotel.locationCity || hotel.city.name } }">{{ hotel.real_city }}酒店</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ hotel.name }}</el-breadcrumb-item>
+      </el-breadcrumb>
+      <HotelDetailNameInfo :hotel="hotel" />
+      <HotelDetailPicsInfo />
+      <HotelDetailProducts :hotelProductsTableData="hotel.products" />
+      <HotelDetailAreaInfo />
+      <HotelDetailAssets :hotel="hotel" />
+      <HotelDetailComments :hotel="hotel" />
+    </div>
   </div>
 </template>
 <script>
@@ -24,7 +33,9 @@ export default {
         location: {},
         city: {},
         products: []
-      }
+      },
+      loadingHotel: false,
+      noneHotelData: true
     }
   },
   methods: {
@@ -32,6 +43,7 @@ export default {
       const [err, res] = await this.$api.getHotels({ id })
 
       if (err) {
+        this.noneHotelData = true
         return
       }
 
@@ -42,15 +54,19 @@ export default {
           location: {},
           city: {}
         }
+        this.noneHotelData = true
         return
       }
 
       this.hotel = res.data.data[0]
+      this.noneHotelData = false
     }
   },
   async mounted () {
+    this.loadingHotel = true
     const id = this.$route.params.id
-    this.renderHotelDetails(id)
+    await this.renderHotelDetails(id)
+    this.loadingHotel = false
   }
 }
 </script>
@@ -63,6 +79,15 @@ export default {
   }
   .height-light {
     color: #f90;
+  }
+  &--loading, &--none {
+    height: 300px;
+  }
+  &--none {
+    padding-top: 100px;
+    text-align: center;
+    font-size: 1.2rem;
+    color: #595959;
   }
 }
 </style>
